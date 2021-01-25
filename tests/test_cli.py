5541a811eba773dbbdc1918d7d86690dd938b14a
harens <harensdeveloper@gmail.com>
2021-01-25 11:26:40 +0000

    Additional clipboard testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from click.testing import CliRunner
+from pytest_mock import MockFixture
 
 from seaport import __version__
 from seaport.init import seaport
@@ -19,10 +20,60 @@ def test_help() -> None:
     assert "Show this message and exit" in result.output
 
 
-def test_clip() -> None:
-    # This is slow...and requires all the deps to be installed
+# The following tests are slow...and requires macports to be installed
+# They could also be more in-depth with their assertions
+# Fortunately, GH Actions doesn't require sudo password
+# py-rich 9.8.0 chosen since all tests/linting pass
+
+
+def test_all() -> None:
     runner = CliRunner()
     result = runner.invoke(
         seaport, ["clip", "py-rich", "--bump", "9.8.0", "--lint", "--test", "--install"]
     )
     assert result.exit_code == 0
+
+
+def test_no_sudo() -> None:
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "py-rich", "--bump", "9.8.0"])
+    assert result.exit_code == 0
+
+
+def test_test_only() -> None:
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "py-rich", "--bump", "9.8.0", "--test"])
+    assert result.exit_code == 0
+
+
+def test_lint_only() -> None:
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "py-rich", "--bump", "9.8.0", "--lint"])
+    assert result.exit_code == 0
+
+
+def test_install_only() -> None:
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "py-rich", "--bump", "9.8.0", "--install"])
+    assert result.exit_code == 0
+
+
+def test_test_fail() -> None:
+    # gping has no tests, so this should fail
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "gping", "--bump", "1.1.0", "--test"])
+    assert result.exit_code == 1
+    assert "Tests failed\n" in result.output
+
+
+def test_lint_fail(session_mocker: MockFixture) -> None:
+
+    # If linting fails
+    session_mocker.patch(
+        "seaport.clipboard.clipboard.perform_lint",
+        return_value=False,
+    )
+
+    runner = CliRunner()
+    result = runner.invoke(seaport, ["clip", "gping", "--bump", "1.1.0", "--lint"])
+    assert result.exit_code == 1
