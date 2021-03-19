4278202db829272e485f7b8eae43649da17171b4
harens <harensdeveloper@gmail.com>
2021-03-19 18:39:02 +0000

    Break down testing

--- scripts/tests/formatting.sh
+++ scripts/tests/formatting.sh
@@ -28,6 +28,8 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+export PREFIX="poetry run python -m "
+
 ${PREFIX} black --diff --check ./
 ${PREFIX} isort --check-only .
 
